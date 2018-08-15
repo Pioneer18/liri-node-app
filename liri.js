91c83e944d08fff0890678152ff83c19688cf57a
Jonathan <sellsj14@gmail.com>
2018-08-14 23:43:58 -0400

    had to fix the if movieName not defined default. was not working before.

--- liri.js
+++ liri.js
@@ -20,7 +20,7 @@ var spotify = new Spotify(keys.spotify);
 var arg1 = process.argv[2];//this will be the liri command
 var arg2 = process.argv[3];//this will be the value passed to the function called by the liri command
 
-//=======================================================================================================
+//===========================================================================================================
 //Liri's functions to respond to user input 
 
 //make the function to `run` the liri bot, and the function to determine what functions to call based on the inputed commands
@@ -52,7 +52,7 @@ var pick = function(caseData, functionData){
 }
 
 
-//=======================================================================
+//===========================================================================================================
 //make the functions that call the spotify api, omdb api, and call the fs module
 
 
@@ -62,12 +62,12 @@ var getArtistName = function(bananas){//you can put anything in here
     return bananas.name; //then attach the name of the prop u wanna get out of the Artists array
 }
 
-
+//===========================================================================================================
 //node-spotify-api function
 var mySpotify = function(songName){
     //if user did not enter a song name default to "The Sign"
     if(songName === undefined){
-        songName = "The Sign";
+        songName = "505"; //one of my favs, also "The Sign" pulls "The Light" - by MihTy,Jeremih,Ty Dolla $ign
     }
     //use the spotify object created on line 14 to call the node-spotify-api
     //npm lists search: function({type: `track1, query: 'your search'}) as the easiest way to make a call
@@ -84,7 +84,6 @@ var mySpotify = function(songName){
         var songs = data.tracks.items; //we will be drilling into songs now
         //songs is an array of objects we will get data from 
         for(var i = 0; i < songs.length; i++){
-            console.log(i);
             //grab the artists name 
             log(chalk.blue("artist(s): ") + chalk.red(songs[i].artists.map(getArtistName)));
             //grab the song name
@@ -95,25 +94,23 @@ var mySpotify = function(songName){
     });    
 }
 
+//===========================================================================================================
 //call the omdb api with the agr2 the user gave to LIRI
 var myMovie = function(functionData){
-    console.log(functionData);
+    if(functionData === undefined){
+        request("http://www.omdbapi.com/?t=mr+nobody&apikey=trilogy", function(error,response,body){
+            //if there is no error and the http status code is 200; which is ok
+            if(!error && response.statusCode === 200){
+                //now make a JSON.parse onto the body so we can read it, then space it out with the 4, null on the reviver?
+                console.log(JSON.parse(body),null, 4);
+            }
+        });
+    }else{
     //setup the query url with the passed in movie name and api key = trilogy
     var queryURL = "http://www.omdbapi.com/?t=" + functionData + "&apikey=trilogy";
 
     //now make the HTTP REQUEST  with the queryURL
     request(queryURL, function(error, response, body){
-        console.log(response.statusCode);
-        //if for some reason user did not enter a movie or spelled it horribly wrong default to Mr. Nobody
-        if(functionData === undefined){
-            request("http://www.omdbapi.com/?t=mr+nobody&apikey=trilogy", function(error,response,body){
-                //if there is no error and the http status code is 200; which is ok
-                if(!error && response.statusCode === 200){
-                    //now make a JSON.parse onto the body so we can read it, then space it out with the 4, null on the reviver?
-                    console.log(JSON.parse(body),null, 4);
-                }
-            });
-        }
         //if user did pass in a movie name then call the api with that
         //also build out a nice terminal log since we care about these results bit more
         if(!error && response.statusCode === 200){
@@ -121,12 +118,33 @@ var myMovie = function(functionData){
             log(chalk.redBright("Movie: ") + JSON.parse(body).Title);
             log(chalk.green("Release Year: ") + JSON.parse(body).year);
             log(chalk.yellow("IMDB Ratings: ") + JSON.parse(body).imdbRating);
-            log(chalk.pink("Rotten Tomatoes: ") + JSON.parse(body).Ratings[1].Value);
+            log(chalk.red("Rotten Tomatoes: ") + JSON.parse(body).Ratings[1].Value);
             log(chalk.blue("Country of production: ") + JSON.parse(body).Country);
             log(chalk.magenta("Language: ") + JSON.parse(body).Language);
             log(chalk.red("Plot: ") + JSON.parse(body).Plot);
             log(chalk.green("Actors: ") + JSON.parse(body).Actors);
         }
+    });
+    }
+}
+
+//===========================================================================================================
+//fs to read and write to files
+var fsThis = function(){
+    //use the readFile method, with utf8 encoding on the random.txt file to `read` its content
+    fs.readFile("random.txt", "utf8", function(err,data){
+        if(err){
+            return console.log(err);
+        }
+        //now use split to break the data at the comma, making the two liri commands
+        var temp = data.split(",");//the split and array build
+        var command1 = temp[0];
+        var command2 = temp[1];
+        //let the user know LIRI successfully got the commands
+        log(chalk.green("your command is: ") + chalk.red(command1) + " " + chalk.blue(command2));
+        //call pick
+
+        pick(command1,command2);
     });
 }
 
